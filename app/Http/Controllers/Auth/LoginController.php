268e0d16c34190d3f49f01ba52a8228e6d0b7528
Jennifer Lawrence <com.widyaop@gmail.com>
2019-04-11 20:23:39 +0800

    Change route

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -23,6 +23,8 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    protected $redirectTo = '/';
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
